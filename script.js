6535a831059a54b4778cee27e122cf70d5a292af
Zahurul Islam <zahurul8259@gmail.com>
2025-07-20 15:52:31 +0600

    Delete specefiq index element of array

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
 let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 let len = data.length;
 
-function insertValue() {
+function removeElement() {
   let pos = parseInt(document.getElementById("position").value);
-  let num = parseInt(document.getElementById("number").value);
   if (!typeof pos == "number" || isNaN(pos)) {
     alert(`position ${pos} is not a number`);
-  }
-  if (!typeof num == "number" || isNaN(num)) {
-    alert(`Given input of number ${pos} is not  a type of number!`);
   } else if (pos < len) {
-    for (let i = len - 1; i >= 0; i--) {
-      if (i >= pos) {
-        data[i + 1] = data[i];
-      }
-      if (i == pos) {
-        data[i] = num;
-        console.log(data);
-        return data;
-      }
+    for (let i = pos; i < len; i++) {
+      data[i] = data[i + 1];
     }
-  } else {
-    data[pos] = num;
+    data.length = data.length - 1;
     console.log(data);
+  } else {
+    alert(`position ${pos} is not valid`);
   }
 }
 
-function spliceValue() {
-  let pos = parseInt(document.getElementById("position").value);
-  let num = parseInt(document.getElementById("number").value);
-  let splicedData = data.toSpliced(pos, 0, num);
-  console.log(splicedData);
-}
 function resetValue() {
   document.getElementById("position").value = "";
   document.getElementById("number").value = "";
