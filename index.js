fb845e4bbdcd7b89dd3e71b416fb35e6766322dd
Zahurul Islam <eco1603021brur@gmail.com>
2026-02-01 14:38:56 +0600

    merge two array in DS & Built in method

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-let array = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
+let array1 = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
+let array2 = [3, 7, 12, 34, 63, 9, 0, 45, 67, 8];
+let array3 = [];
 
-for (let i = 0; i < array.length; i++) {
-  let num = array[i];
-  console.log(num);
+for (let i = 0; i < array1.length; i++) {
+  array3[i] = array1[i];
 }
+for (let i = 0; i < array2.length; i++) {
+  array3[array1.length + i] = array2[i];
+}
+console.log(array3);
 
-//Access specifiq position array;
-let position = 3;
-console.log(`element at position ${position} is : ${array[position]}`);
+//in builtin method easily can do with concat  or ... spread operator
+let array4 = array1.concat(array2);
+let array5 = [...array1, ...array2];
+console.log(array4);
+console.log(array5);
