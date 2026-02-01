abd3b7de78ca088f4e3f3d4731964f5c73b3fd46
Zahurul Islam <eco1603021brur@gmail.com>
2026-02-01 16:22:29 +0600

    array merging by while loop

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ let array1 = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
 let array2 = [3, 7, 12, 34, 63, 9, 0, 45, 67, 8];
 let array3 = [];
 
-for (let i = 0; i < array1.length; i++) {
-  array3[i] = array1[i];
+let d1 = 0;
+let d2 = 0;
+let d3 = 0;
+
+while (d1 < array1.length && d2 < array2.length) {
+  if (array1[d1] < array2[d2]) {
+    array3[d3] = array1[d1];
+    d1++;
+  } else {
+    array3[d3] = array2[d2];
+    d2++;
+  }
+  d3++;
 }
-for (let i = 0; i < array2.length; i++) {
-  array3[array1.length + i] = array2[i];
+
+while (d1 < array1.length) {
+  array3[d3] = array1[d1];
+  d1++;
+  d3++;
 }
 console.log(array3);
-
-//in builtin method easily can do with concat  or ... spread operator
-let array4 = array1.concat(array2);
-let array5 = [...array1, ...array2];
-console.log(array4);
-console.log(array5);
