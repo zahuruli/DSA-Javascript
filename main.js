d3eeed2cdfd3206b49273d4216e4a5be124320a2
Zahurul Islam <78563080+zahuruli@users.noreply.github.com>
2026-02-01 22:05:45 +0600

    array shorting by foe loop

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
+// let shortedArray = data.sort((a, b) => a - b).reverse();
+// console.log(shortedArray);
 let data = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
-let index = undefined;
-function searchElementIndex(el) {
-  console.log(data);
-  for (let i = 0; i <= data.length - 1; i++) {
-    if (data[i] == el) {
-      index = i;
-      break;
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    if (data[j] > data[j + 1]) {
+      temp = data[j];
+      data[j] = data[j + 1];
+      data[j + 1] = temp;
     }
   }
-  if (index == undefined) {
-    console.log(`Element not found`);
-  } else {
-    console.log(`Element ${el} found at position ${index}`);
-  }
 }
-console.log(data.indexOf(0));
-searchElementIndex(0);
+console.log(data);
