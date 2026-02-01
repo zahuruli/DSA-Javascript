19fa8228dc11842d23ca8e796f54893cb4c67a82
Zahurul Islam <78563080+zahuruli@users.noreply.github.com>
2026-02-01 23:23:26 +0600

    Recursion Data Structure in JS

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-// let shortedArray = data.sort((a, b) => a - b).reverse();
-// console.log(shortedArray);
-let data = [9, 45, 2, 8, 45, 23, 7, 78, 0, 11, 41, 77];
-for (let i = 0; i < data.length; i++) {
-  for (let j = 0; j < data.length; j++) {
-    if (data[j] > data[j + 1]) {
-      temp = data[j];
-      data[j] = data[j + 1];
-      data[j + 1] = temp;
-    }
+//basic recursion:
+
+// function myFunction(x) {
+//   console.log(x);
+//   if (x < 10) {
+//     myFunction(x + 1);
+//   }
+// }
+// let data = 0;
+// myFunction(0);
+
+//factorial calculation by recursion:
+
+function factorial(x) {
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
   }
 }
-console.log(data);
+
+console.log(factorial(5));
